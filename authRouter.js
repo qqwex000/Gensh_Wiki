18ab634a33051542e9d7533b481cc3a0103ce12b
karasmar <karasmar759@gmail.com>
2025-03-20 16:48:43 +0100

    hunya

--- authRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const controller = require('./authController');
-
-router.post('/registration', controller.registration)
-router.post('/log' , controller.login)
-router.get('/users' , controller.getUsers)
-
-module.exports = router
